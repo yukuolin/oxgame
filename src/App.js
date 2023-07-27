6d04df95aecdb9f98d183fc173f8a9410470c105
yukuo <130116526+yukuolin@users.noreply.github.com>
2023-07-28 04:24:30 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    const newBoard = [...board];
+    if (calculateWinner(board) || newBoard[index]) return;
+    newBoard[index] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className={`square ${board[index]}`} onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  let status;
+  if (winner) {
+    status = <div className="status status-winner">Winner: {winner}</div>;
+  } else {
+    status = <div className="status">Next player: {xIsNext ? 'X' : 'O'}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {status}
+      <div className="board">
+        {Array(9)
+          .fill()
+          .map((_, index) => renderSquare(index))}
+      </div>
     </div>
   );
+}
+
+function calculateWinner(board) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+  return null;
 }
 
 export default App;
